493ea57341671ef723ca4fc038568a310d36cdba
고석현 <eaglclaws@gmail.com>
2022-10-10 07:37:18 +0000

    MySQL 서버와 express 앱 연결

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 57043
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const port = 57043;
+
+db = mysql.createConnection({
+	host: '127.0.0.1',
+	port: '54855',
+	user: 'develop',
+	password: 'cien14789*',
+	database: 'polling'
+});
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
-})
+});
+
+app.get('/db', (req, res) => {
+	var sql = `SHOW TABLES`;
+	db.query(sql, (err, data, fields) => {
+		if (err) throw err;
+		res.json({
+			status: 200,
+			data,
+			message: "Success"
+		});
+	});
+});
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});

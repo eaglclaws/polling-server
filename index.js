92042e4bb9a8af12f8c07bf396870adacc12dbc7
고석현 <eaglclaws@gmail.com>
2022-10-16 15:07:17 +0000

    Added poll posting request

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ db = mysql.createConnection({
 });
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Hello World!<br><a href="/posts">Test Database!</a>');
 });
 
 app.get('/db', (req, res) => {
@@ -27,6 +27,63 @@ app.get('/db', (req, res) => {
 	});
 });
 
+app.get('/posts', (req, res) => {
+	var sql =
+'SELECT postId, postType, poster as posterId, timeBefore, userCount, storyText, likes, comments, selectionId, selectionText FROM (SELECT aux.*, selectionId, selectionText \
+FROM ( \
+	SELECT postId, postType, posterId, timeBefore, userCount, storyText, likes, comments \
+	FROM ( \
+		SELECT poll.pid as postId, poll.type as postType, poll.uid as posterId, TIMESTAMPDIFF(MINUTE, poll.time, CURRENT_TIMESTAMP()) as timeBefore, COUNT(polldone.uid) as userCount, poll.content as storyText \
+		FROM poll \
+		LEFT JOIN polldone ON poll.pid = polldone.pid \
+		GROUP BY poll.pid \
+	) as userCounter \
+	LEFT JOIN ( \
+		SELECT poll.pid, COUNT(polllikes.uid) as likes \
+		FROM poll \
+		LEFT JOIN polllikes ON poll.pid = polllikes.pid \
+		GROUP BY poll.pid \
+	) as likeCounter ON userCounter.postId = likeCounter.pid \
+	LEFT JOIN ( \
+		SELECT poll.pid, COUNT(comment.cid) as comments \
+		FROM poll \
+		LEFT JOIN comment ON poll.pid = comment.pid \
+		GROUP BY poll.pid \
+	) as commentCounter ON userCounter.postId = commentCounter.pid \
+	ORDER BY timeBefore \
+	LIMIT 2 \
+	OFFSET 0 \
+) as aux \
+LEFT JOIN ( \
+	SELECT selection.pid, selection.sid as selectionId, selection.content as selectionText FROM selection \
+) as optionList ON aux.postId = optionList.pid) as wrapper INNER JOIN (SELECT uid, name as poster FROM user) as u ON u.uid = wrapper.posterId;';
+	db.query(sql, (err, data, fields) => {
+		if (err) throw err;
+		const posts = data.reduce((prev, next) => {
+			if(!Array.isArray(prev)) {
+				prev = [prev];
+			}
+			for (var index in prev) {
+				if (prev[index].postId == next.postId || prev[index].postId == 'pid_' + next.postId) {
+					if(prev[index].selection == null) {
+						prev[index].postId = 'pid_' + prev[index].postId;
+						prev[index].selection = [{selectionId: 'sid_' + prev[index].selectionId, text: prev[index].selectionText}, {selectionId: 'sid_' + next.selectionId, text: next.selectionText}];
+						delete prev[index].selectionId;
+						delete prev[index].selectionText;
+					} else {
+						prev[index].selection.push({selectionId: 'sid_' + next.selectionId, text: next.selectionText});
+					}
+					return prev;
+				}
+			}
+			prev.push(next);
+			return prev;
+		});
+		res.setHeader('Content-Type', 'application/json');
+		res.json({posts: posts});
+	});
+});
+
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Polling server listening on port ${port}`);
 });

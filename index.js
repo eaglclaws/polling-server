b4d996dd36beaf17f2b76f5cdbb57378a079334d
고석현 <eaglclaws@gmail.com>
2022-10-17 12:17:38 +0000

    moved sql queries to separte file, added get for results

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const port = 57043;
+const async = require('async');
+const queries = require('./queries.js');
 
 db = mysql.createConnection({
 	host: '127.0.0.1',
@@ -27,60 +29,57 @@ app.get('/db', (req, res) => {
 	});
 });
 
-app.get('/posts', (req, res) => {
-	var sql =
-'SELECT postId, postType, poster as posterId, timeBefore, userCount, storyText, likes, comments, selectionId, selectionText FROM (SELECT aux.*, selectionId, selectionText \
-FROM ( \
-	SELECT postId, postType, posterId, timeBefore, userCount, storyText, likes, comments \
-	FROM ( \
-		SELECT poll.pid as postId, poll.type as postType, poll.uid as posterId, TIMESTAMPDIFF(MINUTE, poll.time, CURRENT_TIMESTAMP()) as timeBefore, COUNT(polldone.uid) as userCount, poll.content as storyText \
-		FROM poll \
-		LEFT JOIN polldone ON poll.pid = polldone.pid \
-		GROUP BY poll.pid \
-	) as userCounter \
-	LEFT JOIN ( \
-		SELECT poll.pid, COUNT(polllikes.uid) as likes \
-		FROM poll \
-		LEFT JOIN polllikes ON poll.pid = polllikes.pid \
-		GROUP BY poll.pid \
-	) as likeCounter ON userCounter.postId = likeCounter.pid \
-	LEFT JOIN ( \
-		SELECT poll.pid, COUNT(comment.cid) as comments \
-		FROM poll \
-		LEFT JOIN comment ON poll.pid = comment.pid \
-		GROUP BY poll.pid \
-	) as commentCounter ON userCounter.postId = commentCounter.pid \
-	ORDER BY timeBefore \
-	LIMIT 2 \
-	OFFSET 0 \
-) as aux \
-LEFT JOIN ( \
-	SELECT selection.pid, selection.sid as selectionId, selection.content as selectionText FROM selection \
-) as optionList ON aux.postId = optionList.pid) as wrapper INNER JOIN (SELECT uid, name as poster FROM user) as u ON u.uid = wrapper.posterId;';
-	db.query(sql, (err, data, fields) => {
-		if (err) throw err;
-		const posts = data.reduce((prev, next) => {
-			if(!Array.isArray(prev)) {
-				prev = [prev];
-			}
-			for (var index in prev) {
-				if (prev[index].postId == next.postId || prev[index].postId == 'pid_' + next.postId) {
-					if(prev[index].selection == null) {
-						prev[index].postId = 'pid_' + prev[index].postId;
-						prev[index].selection = [{selectionId: 'sid_' + prev[index].selectionId, text: prev[index].selectionText}, {selectionId: 'sid_' + next.selectionId, text: next.selectionText}];
-						delete prev[index].selectionId;
-						delete prev[index].selectionText;
-					} else {
-						prev[index].selection.push({selectionId: 'sid_' + next.selectionId, text: next.selectionText});
+app.get('/posts/:index', (req, res) => {
+	var tasks = [
+		function (callback) {
+			db.query(queries.countAllPosts, (err, row) => {
+				if (err) return callback(err);
+				callback(null, row[0].sum);
+			});
+		},
+		function (sum, callback) {
+			db.query(queries.topTenPosts, [parseInt(req.params.index) * 10], (err, data, fields) => {
+				if (err) return callback(err);
+				const posts = data.reduce((prev, next) => {
+					if(!Array.isArray(prev)) {
+						prev = [prev];
 					}
+					for (var index in prev) {
+						if (prev[index].postId == next.postId || prev[index].postId == 'pid_' + next.postId) {
+							if(prev[index].selection == null) {
+								prev[index].postId = 'pid_' + prev[index].postId;
+								prev[index].selection = [{selectionId: 'sid_' + prev[index].selectionId, text: prev[index].selectionText}, {selectionId: 'sid_' + next.selectionId, text: next.selectionText}];
+								delete prev[index].selectionId;
+								delete prev[index].selectionText;
+							} else {
+								prev[index].selection.push({selectionId: 'sid_' + next.selectionId, text: next.selectionText});
+							}
+							return prev;
+						}
+					}
+					prev.push(next);
 					return prev;
-				}
-			}
-			prev.push(next);
-			return prev;
-		});
+				});
+				var data = {posts: posts, size: sum};
+				callback(null, data);
+			});
+		},
+		function(data, callback) {
+			res.setHeader('Content-Type', 'application/json');
+			res.json(data);
+		}
+	];
+	async.waterfall(tasks, function(err) {
+		if (err) throw err;
+	});
+});
+
+app.get('/result/:poll_id', (req, res) => {
+	var pollId = req.params.poll_id.split('_')[1];
+	db.query(queries.getResult, [pollId], (err, rows) => {
+		if(err) throw err;
 		res.setHeader('Content-Type', 'application/json');
-		res.json({posts: posts});
+		res.json({selectionResult: rows});
 	});
 });
 
